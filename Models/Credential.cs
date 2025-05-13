0371ed4adf8c171ac3e77c979044f37069c92caf
Dharvish <st10275490@vcconnect.edu.za>
2025-05-13 17:31:06 +0200

    Added Credentials

---
+++ Models/Credential.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PROG7311_POE_Part_2.Models
+{
+    public class Credential
+    {
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+}
